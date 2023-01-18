9828c1cf41c16f32fd7f757df4cac3c411ac3941
MeGallin <me@garyallin.uk>
2023-01-18 15:57:54 +0000

    updated db creds

---
+++ controllers/PageHitsController.js
@@ -0,0 +1,20 @@
+const PageHits = require('../models/PageHitsModel');
+const requestIp = require('request-ip');
+
+exports.pageHits = async (req, res, next) => {
+  const ipAddress = requestIp.getClientIp(req);
+  const hits = await PageHits.find();
+
+  const ipAddressExists = await PageHits.findOne({ ipAddress: ipAddress });
+  try {
+    if (!ipAddressExists) {
+      const newIpAddress = new PageHits({
+        ipAddress,
+      });
+      await newIpAddress.save();
+    }
+    res.status(200).json({ success: true, hits });
+  } catch (error) {
+    next(error);
+  }
+};
