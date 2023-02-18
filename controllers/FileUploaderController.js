832b913eb39d8e9c3ee9aa45e50f46b7a41768f8
MeGallin <me@garyallin.uk>
2023-02-18 11:24:36 +0000

    End points

---
+++ controllers/FileUploaderController.js
@@ -0,0 +1,34 @@
+const ErrorResponse = require('../utils/errorResponse');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+// @description: Get the contents of the news file
+// @route: GET /api/my-news
+// @access: PUBLIC
+exports.readMyNews = async (req, res, next) => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, '../' + 'routes/' + 'upload', 'news.txt'),
+      'utf8',
+    );
+
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
+// @description: Get the contents of the recent past text file
+// @route: GET /api/recent-past
+// @access: PUBLIC
+exports.readPresently = async (req, res, next) => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, '../' + 'routes/' + 'upload', 'presently.txt'),
+      'utf8',
+    );
+
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    next(error);
+  }
+};
